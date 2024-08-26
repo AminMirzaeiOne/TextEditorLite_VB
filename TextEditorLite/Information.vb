2cf1cb0e75971702852e4a35b531bf73de712ac2
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-27 03:10:34 +0330

    - Update Ui Information Module

--- TextEditorLite/Information.vb
+++ TextEditorLite/Information.vb
@@ -1,3 +1,34 @@
 ﻿Module Information
-
+    Public Sub DrawInfo()
+        Console.Clear()
+        System.Console.ForegroundColor = ConsoleColor.DarkCyan
+        Console.WriteLine("   ╔═══════════════════════════════════╗")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║     Programmer : Amin Mirzaei     ║ Amin Mirzaei, programmer and developer of this software project ")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ╚═══════════════════════════════════╝")
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        Console.WriteLine("   --------------------------------------")
+        System.Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("   ╔═══════════════════════════════════╗")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║ Github(Username) : AminMirzaeiOne ║ You can follow me on GitHub and see all my software projects ")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ╚═══════════════════════════════════╝")
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        Console.WriteLine("   --------------------------------------")
+        System.Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("   ╔═══════════════════════════════════╗")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║     Instagram : amin.reals        ║ You can follow me on Instagram ")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ║                                   ║")
+        Console.WriteLine("   ╚═══════════════════════════════════╝")
+        System.Console.ResetColor()
+    End Sub
 End Module
