6b105b40cba878cd336fcfb7fd87f1b99689cdd8
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-27 02:55:01 +0330

    Update Program Module

--- TextEditorLite/Program.vb
+++ TextEditorLite/Program.vb
@@ -1,7 +1,80 @@
 ﻿Module Program
 
     Sub Main()
+        Console.Clear()
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.BackgroundColor = ConsoleColor.Cyan
+        Dim logo = "Text Editor | v1.0"
+        Console.SetCursorPosition((Console.WindowWidth - logo.Length) / 2, Console.CursorTop)
+        Console.WriteLine(logo)
 
+
+        Console.WriteLine("   Enter Your Current Text   " & vbLf)
+        Console.Write(">>:")
+        Dim initialUserInput As String = Console.ReadLine()
+        Dim myTextEditor As TextEditor = New TextEditor(initialUserInput)
+
+
+        While True
+            Console.SetCursorPosition((Console.WindowWidth - logo.Length) / 2, Console.CursorTop)
+            Console.WriteLine(logo)
+            Console.WriteLine(vbLf & "Enter your preference" & vbLf)
+            Console.WriteLine("[1] Add " & vbLf & "[2] Clear " & vbLf & "[3] Remove " & vbLf & "[4] Undo" & vbLf & "[5] Exit" & vbLf & vbLf)
+            Console.Write(">>:")
+
+            Dim userOption As String = Console.ReadLine()
+
+
+            If Equals("1", userOption) Then
+#Region "Single Operation"
+
+                Console.Clear()
+
+                ' Getting User Input to Add and Constructing Append Class Instance
+                Console.WriteLine("You Selected to Add String to Current Text" & vbLf & "Please Enter the Text" & vbLf)
+                Dim textToAdd As String = Console.ReadLine()
+                Dim onGoingAppend As Append = New Append(textToAdd)
+
+                ' Calling Methods from TextEditor Class
+                myTextEditor.ExecuteICommand(onGoingAppend)
+                myTextEditor.DisplayProcessed()
+                Console.WriteLine(vbLf & "Please Press Any key to Continue....")
+                Console.ReadKey()
+                Console.Clear()
+
+#End Region
+                myTextEditor.DisplayCurrentText()
+
+            ElseIf Equals("2", userOption) Then
+#Region "Single Operation"
+
+                Console.Clear()
+
+                ' Getting User Input to Add and Constructing Append Class Instance
+                Console.WriteLine("You Selected to Add String to Current Text" & vbLf & "Please Enter the Text" & vbLf)
+                Dim clear As Clear = New Clear(myTextEditor.currentText)
+
+                ' Calling Methods from TextEditor Class
+                myTextEditor.ExecuteICommand(clear)
+                myTextEditor.DisplayProcessed()
+                Console.WriteLine(vbLf & "Please Press Any key to Continue....")
+                Console.ReadKey()
+                Console.Clear()
+
+#End Region
+                myTextEditor.DisplayCurrentText()
+            ElseIf Equals("3", userOption) Then
+#Region "Single Operation"
+
+                Console.Clear()
+
+                ' Getting User Input to Add and Constructing Append Class Instance
+                Console.WriteLine("You Selected to Remove String to Current Text" & vbLf & "Please Enter the number of text to remove" & vbLf)
+                Dim indexToRemove As String = Console.ReadLine()
+                Dim toRemove As BackSpace = New BackSpace(Integer.Parse(indexToRemove), myTextEditor.currentText)
+
+
+        End While
     End Sub
 
 End Module
