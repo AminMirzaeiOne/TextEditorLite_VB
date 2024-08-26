7733f5b4aeb805ab3440cae9593cbe5a322e0fc4
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-27 02:24:37 +0330

    Create BackSpace Class

---
+++ TextEditorLite/BackSpace.vb
@@ -0,0 +1,25 @@
+﻿Public Class BackSpace
+    Implements ICommand
+
+    Private toBeRemovedChars As Integer
+
+    Private OriginalString As String
+
+    Public Sub New(toBeRemovedChars As Integer, originalString As String)
+        Me.toBeRemovedChars = toBeRemovedChars
+        Me.OriginalString = originalString
+    End Sub
+
+    Public Function TextToExecute(execute As String) As String Implements ICommand.TextToExecute
+        If execute.Length >= toBeRemovedChars Or toBeRemovedChars <> 0 Then
+            Return (execute.Remove((execute.Length) - (toBeRemovedChars), toBeRemovedChars))
+        Else
+            Return ($"Please enter a number below \n>>:{execute.Length}")
+        End If
+    End Function
+
+    Public Function TextToUndo(undo As String) As String Implements ICommand.TextToUndo
+        Throw New NotImplementedException()
+    End Function
+
+End Class
