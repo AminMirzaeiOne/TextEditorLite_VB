f7efe98b38fdf2e01a6f5c797c20d7a695ed2217
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-27 02:48:43 +0330

    - Create Clear Class

---
+++ TextEditorLite/Clear.vb
@@ -0,0 +1,18 @@
+﻿Public Class Clear
+    Implements ICommand
+
+    Private previousText As String
+
+    Public Sub New(value As String)
+        Me.previousText = value
+    End Sub
+
+    Public Function TextToExecute(execute As String) As String Implements ICommand.TextToExecute
+        previousText = execute
+        Return ""
+    End Function
+
+    Public Function TextToUndo(undo As String) As String Implements ICommand.TextToUndo
+        Return previousText
+    End Function
+End Class
